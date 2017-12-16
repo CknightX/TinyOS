6e8875e42e162fa4cb9fdad8f70db7b8ce8eb3e5
CknightX <caokeck123@163.com>
2017-12-16 16:54:32 +0800

    print segment addr in memory

--- kernel/main.c
+++ kernel/main.c
@@ -11,6 +11,12 @@ extern void set_8253();
 extern void init_keyboard();
 extern void init_clock();
 
+/*link.ld*/
+extern uint8_t kernel_start[];
+extern uint8_t kernel_end[];
+extern uint8_t code[];
+extern uint8_t data[];
+
 void test1()
 {
 	while(1)
@@ -41,6 +47,11 @@ void init()
 {
 	init_clock();
 	printk("OS now\n");
+	printk("kernel start:  0x%08X\n",kernel_start);
+	printk("code start:    0x%08X\n",code);
+	printk("data start:    0x%08X\n",data);
+	printk("kernel end:    0x%08X\n",kernel_end);
+	printk("kernel used:   %d KB\n\n",(kernel_end-kernel_start+1023)/1024);
 	p_proc_ready=proc_table;
 
 
@@ -53,6 +64,7 @@ int _osmain(void)
 {
 	init();
 	init_keyboard();
+
 	restart(); //进入进程
 	while(1){}
 }
